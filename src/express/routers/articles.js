bd3ec7355d911a5239087f54a12029da0bfcba3d
akrjuger <krjuger@ya.ru>
2021-02-14 09:18:37 +0300

    add some renders + article adding

--- src/express/routers/articles.js
+++ src/express/routers/articles.js
@@ -3,33 +3,74 @@
 /**
  * Роутер для статей. ('/articles')
  */
-
 const {Router} = require(`express`);
 
-const offersRoutes = new Router();
+const api = require(`../api`).getDefaultAPI();
+
+const Uploader = require(`../lib/uploader`);
+
+const articlesRoutes = new Router();
+const uploaderMiddleware = new Uploader(`img`).getMiddleware();
 
 /**
  * Обработка маршрута для добавления статьи - открывается пустая форма для редактирования
  */
-offersRoutes.get(`/add`, (req, res) => res.render(`pages/articles/edit-article`));
+articlesRoutes.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`pages/articles/edit-article`, {article: {}, categories, isNew: true});
+});
+
+/**
+ * Принятие новой статьи
+ */
+articlesRoutes.post(`/add`, uploaderMiddleware.single(`file`), async (req, res) => {
+  const {body, file} = req;
+  const articleData = {
+    image: file ? file.filename : body.image,
+    title: body.title,
+    announce: body.announce,
+    text: body.text,
+    categories: body.categories
+  };
+  console.log(body);
+  console.log(articleData);
+  console.log(file.filename);
+  // console.log(articleData);
+  try {
+    await api.createArticle(articleData);
+    res.redirect(`/my`);
+  } catch (e) {
+    res.redirect(`back`);
+  }
+});
 
 
 /**
  * Обработка маршрута для статьи
  */
-offersRoutes.get(`/:id`, (req, res) => res.render(`pages/articles/article`));
+articlesRoutes.get(`/:id`, (req, res) => res.render(`pages/articles/article`));
 
 
 /**
  * Обработка маршрута для редактирования статьи
  */
-offersRoutes.get(`/edit/:id`, (req, res) => res.send(`pages/articles/edit-article`));
+articlesRoutes.get(`/edit/:id`, async (req, res) => {
+  let article;
+  let categories;
+  try {
+    [article, categories] = await Promise.all([api.getArticle(req.params[`id`]), api.getCategories()]);
+    res.render(`pages/articles/edit-article`, {article, categories});
+  } catch (err) {
+    req.log.info(`Article is not found`);
+    res.redirect(`/404`);
+  }
+});
 
 
 /**
  * Обработка маршрута для категории
  */
-offersRoutes.get(`/category/:id`, (req, res) => res.render(`pages/articles/articles-by-category`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.render(`pages/articles/articles-by-category`));
 
 
-module.exports = offersRoutes;
+module.exports = articlesRoutes;
--- src/express/routers/my.js
+++ src/express/routers/my.js
@@ -8,18 +8,38 @@
 
 const {Router} = require(`express`);
 
+const api = require(`../api`).getDefaultAPI();
+
 const myRoutes = new Router();
 
 /**
  * Обработка маршрута для админской страницы с статьями
  */
-myRoutes.get(`/`, (req, res) => res.render(`pages/my/articles`));
+myRoutes.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`pages/my/articles`, {articles});
+});
 
 
 /**
  * Обработка маршрута для админской страницы с комментариями
  */
-myRoutes.get(`/comments`, (req, res) => res.render(`pages/my/comments`));
+myRoutes.get(`/comments`, async (req, res) => {
+  const articles = await api.getArticles();
+
+  // TEMPORARY отдаются не комментарии по объявлению пользователя, а просто комментарии трех последних статей.
+  const comments = [];
+  articles.slice(0, 3)
+    .forEach((article) => {
+      article.comments.forEach((comment) => comments.push({
+        articleTitle: article.title,
+        articleId: article.id,
+        ...comment
+      }));
+    });
+
+  res.render(`pages/my/comments`, {comments});
+});
 
 
 /**
