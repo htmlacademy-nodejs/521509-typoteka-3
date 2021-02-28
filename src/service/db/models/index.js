cccbdf2e82089aa2e774a7f0396b6941f58e8420
akrjuger <krjuger@ya.ru>
2021-02-28 10:57:37 +0300

    add user model and migration for it

--- src/service/db/models/index.js
+++ src/service/db/models/index.js
@@ -7,6 +7,7 @@ const defineCategory = require(`./category`);
 const defineArticle = require(`./article`);
 const defineComment = require(`./comment`);
 const defineArticleCategory = require(`./article-category`);
+const defineUser = require(`./user`);
 const Alias = require(`./aliase`);
 
 module.exports = (db) => {
@@ -17,22 +18,23 @@ module.exports = (db) => {
   const Article = defineArticle(db);
   const Comment = defineComment(db);
   const ArticleCategory = defineArticleCategory(db);
+  const User = defineUser(db);
 
   /**
    * Настраиваем связи
    */
 
   /**
-   * Связь объявление - комментарий
-   * один ко многим, у объявления много комментариев, а комментарий к одному только объявлению.
+   * Связь статья - комментарий
+   * один ко многим, у статьи много комментариев, а комментарий к одной только статье.
    */
   Article.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `article_id`});
   Comment.belongsTo(Article, {as: Alias.ARTICLES, foreignKey: `article_id`});
 
 
   /**
-   * Связь "категории - объявления".
-   * Многие ко многим, так как у одной категории может быть много объявлений, а у одного объявления много категорий.
+   * Связь "категории - статьи".
+   * Многие ко многим, так как у одной категории может быть много статей, а у одной статьи много категорий.
    * Используется связь  super-many-to-many https://sequelize.org/master/manual/advanced-many-to-many.html
    */
   Category.belongsToMany(Article, {
@@ -50,11 +52,25 @@ module.exports = (db) => {
   Category.hasMany(ArticleCategory, {as: Alias.ARTICLES_CATEGORIES, foreignKey: `category_id`});
   Article.hasMany(ArticleCategory, {as: Alias.ARTICLES_CATEGORIES, foreignKey: `article_id`});
 
+  /**
+   * Связь пользователь - статья
+   * один ко многим, у пользователя много статей, а статья к одному только пользователю.
+   */
+  User.hasMany(Comment, {as: Alias.ARTICLES, foreignKey: `user_id`});
+  Article.belongsTo(Article, {as: Alias.USERS, foreignKey: `user_id`});
+
+  /**
+   * Связь пользователь - комментарий
+   * один ко многим, у пользователя много комментариев, а комментарий к одному только пользователю.
+   */
+  User.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `user_id`});
+  Comment.belongsTo(Article, {as: Alias.USERS, foreignKey: `user_id`});
+
 
   return {
     Category,
     Article,
     Comment,
-    // OfferCategory
+    User
   };
 };
