cfa641828ea36e9057e61094053391f7ae176110
akrjuger <krjuger@ya.ru>
2021-03-02 06:48:08 +0300

    add middlewares for auth checking

---
+++ src/express/middlewares/check-user-auth.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Проверка, что пользователь авторизован.
+ *
+ * !!
+ * Это фронт-сервис, тут payload не проверяется. Если пользователь подделает payload, то вероятно в шапке отразится, что он супер админ.
+ * Но это не критично, так как при первом же запросе к приватному API токен будет проверен.
+ *
+ * Плюс такого решения - меньше запросов к API.
+ * !!
+ */
+const API = require(`../api`);
+
+const atob = require(`atob`);
+const api = API.getDefaultAPI();
+
+
+module.exports = async (req, res, next) => {
+  try {
+    // получаем токены из кук
+    let {accessToken, refreshToken} = JSON.parse(req.cookies.tokens);
+
+    // смотрим, что находится в payload в токенах
+    let payloadAccess = JSON.parse(atob(accessToken.split(`.`)[1]));
+    const payloadRefresh = JSON.parse(atob(refreshToken.split(`.`)[1]));
+
+    // если оба токена вышли по сроку, то выбрасываем ошибку.
+    if (payloadAccess.exp * 1000 < Date.now() && payloadRefresh.exp * 1000 < Date.now()) {
+      throw new Error(`Tokens are expired.`);
+    }
+
+    // если access токен вышел по сроку, а refresh нет, то обновляем токены.
+    if (payloadAccess.exp * 1000 < Date.now() && payloadRefresh.exp * 1000 > Date.now()) {
+
+      // пробуем рефрешнуть
+      ({accessToken, refreshToken} = await api.refreshAuthUser({refreshToken}));
+
+      // запихиваем в куки, чтобы сохранились у пользователя
+      res.cookie(`tokens`, JSON.stringify({accessToken, refreshToken}), {httpOnly: true});
+
+      // парсим только токен access чтобы достать пользователя
+      payloadAccess = JSON.parse(atob(accessToken.split(`.`)[1]));
+    }
+
+    // запоминаем пользователя
+    res.locals.user = payloadAccess.data;
+    res.locals.accessToken = accessToken;
+    res.locals.isLogged = true;
+  } catch (e) {
+    // если что-то крешнулось, то считаем пользователя не авторизованным.
+    req.log.debug(e);
+    res.locals.isLogged = false;
+    res.locals.user = {};
+  }
+
+  next();
+};
