46bdf4283f793781999db612154fd2eb998e1f76
akrjuger <krjuger@ya.ru>
2021-03-11 09:53:18 +0300

    add websocket.io to backend

---
+++ src/service/lib/web-socket.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const socket = require(`socket.io`);
+
+const Logger = require(`../../lib/logger`);
+
+class WebSocket {
+  constructor(server) {
+    this._logger = new Logger(`web-sockets`).getLogger();
+    this._socket = socket(server, {
+      cors: {
+        origin: `${process.env.FRONT_SERVICE_DOMAIN_URL}:${process.env.FRONT_SERVICE_PORT}`
+      }
+    });
+
+    this._init();
+  }
+
+  _init() {
+    this._socket.on(`connection`, (client) => {
+      const {address: ip} = client.handshake;
+
+      this._logger.info(`Новое подключение к WebSocket: ${ip}`);
+
+      client.on(`disconnect`, () => {
+        this._logger.info(`Клиент отключён: ${ip}`);
+      });
+    });
+  }
+
+  emit(eventName, param) {
+    try {
+      this._socket.emit(eventName, param);
+    } catch (error) {
+      this._logger.error(`Something wrong with socket.io, see info: ${error}`);
+    }
+  }
+
+  static getSocketsForTests() {
+    return {
+      emit() {}
+    };
+  }
+
+
+}
+
+module.exports = WebSocket;
